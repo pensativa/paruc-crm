dc9b247181ed33bc5607ae0baf439503f4a572f2
Mariia <mkofein@gmail.com>
2022-04-17 21:58:52 +0300

    Add all pages

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,11 +1,16 @@
+//Инициация всех подсказок
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 });
 
+//Показать пароль
 (function() {
     const showPasswordButton = document.querySelector('.show-password');
     const inputOfPassword = document.getElementById('singin-password');
+    if (!showPasswordButton) {
+        return;
+    }
     showPasswordButton.onclick = function() {
         if (showPasswordButton.classList.contains('open')) {
             inputOfPassword.setAttribute('type', 'password');
@@ -15,4 +20,33 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
             showPasswordButton.classList.add('open');
         }
     }
+  })();
+
+  //Tovar checkbox
+  (function() {
+    const checkbox = document.getElemenById('select');
+    if (!checkbox) {
+        return;
+    }
+    checkbox.oninput = function() {
+        
+    }
+  })();
+  (function() {
+    const checkboxes = document.querySelectorAll('.oder__table--tovar input[type="checkbox"]');
+    const select = document.querySelector('.section__block-select');
+    if (!select) {
+        return;
+    }
+    checkboxes.forEach(element => {
+        element.oninput = function() {
+            const checkedCheckboxes = document.querySelectorAll('.oder__table--tovar input[type="checkbox"]:checked');
+            console.log(checkedCheckboxes);
+            if (checkedCheckboxes.length > 0) {
+                select.style.opacity = '1';
+            } else {
+                select.style.opacity = '0';
+            }
+        }
+    });
   })();
